dc5e5380223470d7af91f82ebd1f7f45bcd507b3
João Rodrigues <joaorodriguesdiasneto@gmail.com>
2022-03-18 02:59:00 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  //Swagger Settings
+
+  const config = new DocumentBuilder()
+    .setTitle('Real-time chat app backend')
+    .setDescription(
+      'A real-time chat app backend that allows users to chat with each other using websockets.',
+    )
+    .setVersion('0.1')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
