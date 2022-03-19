2d54d645e4ae98e4835af40da21c939dd5027c54
João Rodrigues <joaorodriguesdiasneto@gmail.com>
2022-03-19 17:37:26 -0300

    feat: create userWithoutPassword type

--- src/user/dto/user-without-password.dto.ts
+++ src/user/dto/user-without-password.dto.ts
@@ -1,18 +1,18 @@
-export class userWithoutPasswordDto {
-  name: string;
-  username: string;
-  email: string;
-  profilePhoto: string | null;
-  birthDate: Date;
-  cpf: string;
-  cep: string | null;
-  country: string | null;
-  state: string | null;
-  city: string | null;
-  address: string | null;
-  phonenumber: string | null;
+export class messages {
+  id: string;
+  text: string;
   createdAt: Date;
-  updatedAt: Date;
-  active: boolean;
-  deleted: boolean;
+  ownerUsername: string;
+  roomName: string;
+}
+export class userWithoutPasswordDto {
+  id?: string;
+  name?: string;
+  username?: string;
+  email?: string;
+  isLoggedIn?: boolean;
+  messages?: messages[];
+  deleted?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
